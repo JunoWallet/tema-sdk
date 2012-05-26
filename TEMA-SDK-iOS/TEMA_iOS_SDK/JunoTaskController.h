41ad1eea66a2d85a38dbed7a63cd9d4c6a7ccef0
Jae Kim <jae_kim@junowallet.com>
2012-05-25 19:01:29 -0700

    add src protection - hide sign keys etc

--- TEMA-SDK-iOS/TEMA_iOS_SDK/JunoTaskController.h
+++ TEMA-SDK-iOS/TEMA_iOS_SDK/JunoTaskController.h
@@ -2,20 +2,18 @@
 //  JunoTaskController.h
 //  JunoTaskController
 //
-//  Created by Jae Kim on 1/13/12.
 //  Copyright (c) 2012 Mobile Media Solutions, Inc. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface JunoTaskController : NSObject {
-    NSString *jtcAppId;
-    NSString *evtName;
+    //
 }
-@property (nonatomic, retain) NSString *jtcAppId;
-@property (nonatomic, retain) NSString *evtName;
 
 +(void) trackPayPerEngagement:(NSString*)advKey;
 +(void) trackPayPerEngagement:(NSString*)advKey withEvent:(NSString*)evtName;
++(void) trackPayPerEngagement:(NSString*)advKey withMx:(int)mm;
++(void) trackPayPerEngagement:(NSString*)advKey withEvent:(NSString*)evtName withMx:(int)mm;
 
 @end
